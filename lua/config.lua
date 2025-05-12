8e9daca290b1606286e88d4f3c97e501fefae7c1
Jose Moreno <jamm.webdev@gmail.com>
2025-05-12 01:37:59 -0400

    more pleasant white color for text

--- lua/config.lua
+++ lua/config.lua
@@ -3,7 +3,7 @@ vim.o.relativenumber = true
 vim.o.wrap = false
 vim.o.shiftwidth = 1
 vim.o.laststatus = 3
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", fg = "#d5c4a1" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", fg = "#ebdbb2" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE", fg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", fg = "NONE" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE", fg = "NONE" })
