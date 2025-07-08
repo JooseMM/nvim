f5446ab15222639a37bdf557a3de8c017c734f07
Jose Moreno <jamm.webdev@gmail.com>
2025-07-07 23:48:44 -0400

    fix(LSP): improve logic to handle LSP better and other plugins

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -2,18 +2,32 @@ return {
 	{
 		"VonHeikemen/lsp-zero.nvim",
 		branch = "v3.x",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+		},
 		config = function()
 			local lsp_zero = require("lsp-zero")
-			local lsp_config = require("lspconfig")
+			local lspconfig = require("lspconfig")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 			lsp_zero.extend_lspconfig()
-			lsp_config.lua_ls.setup({})
+
+			-- Add cmp capabilities to all servers
+			local capabilities = cmp_nvim_lsp.default_capabilities()
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+
+			lspconfig.angularls.setup({
+				filetypes = { "html", "angular" },
+				capabilities = capabilities,
+			})
 
 			lsp_zero.on_attach(function(client, bufnr)
 				lsp_zero.default_keymaps({ buffer = bufnr })
 			end)
 		end,
 	},
-	{ "neovim/nvim-lspconfig" },
-	{ "L3MON4D3/LuaSnip" },
 }
